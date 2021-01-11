9da36e7030b81315919bb96cee11851252f9d8c8
Jens Eeckhout <jenseeckhout2009@hotmail.com>
2021-01-11 11:36:10 +0100

    tested out using variable with external call

---
+++ js/ArGame.js
@@ -0,0 +1,11 @@
+let number = 0;
+function Test()
+{
+ console.log("Test function has been called succesfully");
+ number = 5;
+}
+
+function LogNumber()
+{
+ console.log("Printed number ", number);
+}
